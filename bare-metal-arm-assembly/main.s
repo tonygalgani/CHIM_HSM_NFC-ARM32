e08b0cf816b7fe7f51d5dfb71892a475ae0944f5
UrDeadra <hermamora@tuta.io>
2024-10-12 17:12:38 +0000

    feat: enable clock_gpioa in main.s

--- bare-metal-arm-assembly/main.s
+++ bare-metal-arm-assembly/main.s
@@ -9,21 +9,33 @@
 .section .text
 .global reset_handler
 
-.include "gpio_address_map.s"
-  /* pin PA5 = onboard LED */
-  /* Bus AHB2 0x4800 0000 - 0x4800 03FF 1 KB GPIOA */
-  /* Bus AHB1 0x4002 1000 - 0x4002 13FF 1 KB RCC */
+.include "port_defs.s"
+.include "gpio_defs.s"
+
+@ pin PA5 = onboard LED
 
 reset_handler:
-  /* Starting point of the program */
-  ldr r0, =GPIOA_MODER        /* Load address of GPIOA_MODER */
-  ldr r1, [r0]                /* Read the current value */
-  ldr r2, =0x00000C00         /* Load bit Mask into r2 (set bit 10 & 11)*/
-  bic r1, r1, r2              /* Clear bits 10 and 11 (PA5 mode) */
-  ldr r2, =0x00000A00
-  orr r1, r1, r2              /* Set PA5 to output mode (01) */
-  str r1, [r0]                /* Write back to GPIOA_MODER */
+@ Starting point of the program
+
+  ldr r1, =GPIOA_OSPEEDR
+  ldr r2, =PA5_SPEED_MASK
+  ldr r3, =PA5_SPEED_MEDIUM
+  bl modify_register
+
+  ldr r1, =GPIOA_MODER
+  ldr r2, =PA5_MODE_MASK
+  ldr r3, =PA5_MODE_OUTPUT
+  bl modify_register
 
 infinite_loop:
-    b infinite_loop                /* Infinite looping */
+  ldr r1, =
+  ldr r2, =PA5_MODE_MASK
+  ldr r3, =PA5_MODE_OUTPUT   
+  b infinite_loop           @ Infinite looping
 
+modify_register:
+  ldr r4, [r1]                @ Read the register's content
+  bic r4, r4, r2              @ Clear bits using mask
+  orr r4, r4, r3              @ Set bit value
+  str r4, [r1]                @ Write back the value into the register 
+  bx lr
